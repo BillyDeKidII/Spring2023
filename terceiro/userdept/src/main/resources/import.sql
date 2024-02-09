3166dc826bc5f96a9eb17ace4188a9193c7139a4
BillyDeKidII <thiaroll@gmail.com>
2024-02-09 10:38:40 -0300

    Final da etapa. Configuração do banco de dados H2

---
+++ terceiro/userdept/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Thiago', 'thiago@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Rafael', 'rafael@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Silva', 'thiago@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Romão', 'romao@gmail.com');
